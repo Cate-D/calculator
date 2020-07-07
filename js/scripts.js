9577b141d90c6bdd16b53b01b507d9eca14e4985
Cate Draney <catiedraney@gmail.com>
2020-07-07 16:04:09 -0700

    update code to Intro to Programming (Evening) JavaScript & jQuery - 3 Calculator User Interface lession 10 of 13.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,29 @@
+// Business logic:
+
 function add(number1, number2) {
   return number1 + number2;
-} 
+}
 
 function subtract(number1, number2) {
   return number1 - number2;
-} 
+}
 
 function multiply(number1, number2) {
   return number1 * number2;
-} 
+}
 
 function divide(number1, number2) {
   return number1 / number2;
-} 
-const number1 = parseInt (prompt("Enter a number"))
-const number2 = parseInt (prompt("Enter another number"))
-const result = divide(number1, number2);
-alert(result);  
-
-function temp(farenh) {
-  return ((farenh - 32)*5)/9;
 }
 
-const farenh = parseInt(prompt("Enter a temperature in Farenheit"));
-const celsius = temp(farenh).toFixed(2);
-alert("That temperature in Celsius is " + celsius +" degrees"); 
+// User interface logic:
 
-function tbsToMl(tablespoon) {
-  return (tablespoon * 14.7868)
-} 
-
-const tablespoon = parseInt(prompt("Enter tablespoon amount"));
-const ml = tbsToMl(tablespoon);
-alert("tablespoon in ml is" + ml +"ml");
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+   event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
